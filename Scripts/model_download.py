0bb7fbf0e30c437be0e91d44125069e4033ede41
quic-mvijayak <quic_mvijayak@quicinc.com>
2025-06-12 18:08:31 +0530

    Update model_download.py
    Signed-off-by: quic-mvijayak <quic_mvijayak@quicinc.com>

--- Scripts/model_download.py
+++ Scripts/model_download.py
@@ -7,12 +7,13 @@
 # =============================================================================
 
 # Use a pipeline as a high-level helper
+import torch
 from transformers import pipeline
 pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B-Instruct")
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", torch_dtype=torch.float16)
 import os
 os.makedirs("llama_model")
 tokenizer.save_pretrained("llama_model/")
